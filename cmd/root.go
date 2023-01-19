6391bf9e34ccd3e75d12005b59590abe77e17401
artist321 <aadesu+jid@gmail.com>
2023-01-19 23:09:57 +0600

    hello msg

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 Artem Demchenko a.a.demchenko@yandex.com
-
 */
 package cmd
 
@@ -10,18 +9,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "myapp",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "protocol.M2",
+	Short: "Многофункциональная программа для метрологов",
+	Long: `Ничего НЕ ГАРАНТИРУЕТСЯ, в пределах, ограниченных законом.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Сообщения об ошибках и вопросы отправляйте на <a.a.demchenko@yandex.com>
+Чтобы узнать как пользоваться программой, введите:	protocol.M2 help`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +42,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
